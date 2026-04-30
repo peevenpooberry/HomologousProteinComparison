58660c9666c241a090d77c1fa3453571c262cb33
Steven Engelbretson <sje779@my.utexas.edu>
2026-04-30 23:55:15 +0000

    finished main workflow

--- main_workflow/calc_stats.py
+++ main_workflow/calc_stats.py
@@ -2,7 +2,9 @@
 
 import subprocess
 import sys
+import json
 from pathlib import Path
+import shutil
 from typing import Optional
 
 import numpy as np
@@ -47,9 +49,10 @@ WEIGHT_P2RANK = 0.5
 class ProteinFile(BaseModel):
     model_config = {"arbitrary_types_allowed": True}
 
-    protein_name: str
+    file_name: str
     file_path: Path
     sequence: list[str]
+    sequence_to_alignment_map: dict = {}
     PLDDT_per_res: list[float] = []
     PLDDT_scores: list[float] = []
     P2Rank_per_res: list[float] = []
@@ -61,6 +64,7 @@ class Session(BaseModel):
     name: str
     final_score: list[float] = []
     MSA_path: Optional[str] = None
+    P2Rank_output_path: Optional[str] = None
     sequence_conservation: list[float] = []
     PLDDT_score_conservation: list[float] = []
     P2Rank_score_conservation: list[float] = []
@@ -80,12 +84,12 @@ def parse_structure_files(input_dir, aa_map):
     for file in input_dir.iterdir():
         structure = None
         if file.is_file:
-            protein_name = file.stem
+            file_name = file.stem
             with open(file, "r") as f:
                 if str(file).endswith(".pdb"):
-                    structure = pdb_parser.get_structure(protein_name, f)
+                    structure = pdb_parser.get_structure(file_name, f)
                 elif str(file).endswith(".cif"):
-                    structure = cif_parser.get_structure(protein_name, f)
+                    structure = cif_parser.get_structure(file_name, f)
         
         if structure:
         # for each structure need to get
@@ -110,7 +114,7 @@ def parse_structure_files(input_dir, aa_map):
                             plddt = ca.get_bfactor()
                             plddt_per_res.append(plddt)
             
-            protein = ProteinFile(protein_name=protein_name,
+            protein = ProteinFile(file_name=file_name,
                                   file_path=filepath,
                                   sequence=sequence,
                                   PLDDT_per_res=plddt_per_res)
@@ -124,6 +128,7 @@ def parse_structure_files(input_dir, aa_map):
 
 def generate_fasta(work_dir, session):
     work_dir = Path(work_dir)
+    work_dir.mkdir(exist_ok=True)
     fasta_path = work_dir.joinpath("muscle_input.fasta")
 
     proteins = session.proteins
@@ -134,7 +139,7 @@ def generate_fasta(work_dir, session):
             for res in protein.sequence:
                 sequence += res
 
-            file.write(f">{protein.protein_name}\n")
+            file.write(f">{protein.file_name}\n")
             file.write(f"{sequence}\n")
 
 
@@ -144,19 +149,30 @@ def muscle_command(work_dir, muscle_exe, session):
     output_fasta = work_dir.joinpath(f"{session.name}_msa.fasta")
 
     try:
-        subprocess.run([muscle_exe, "-align", fasta_path, "-output", output_fasta], check=True)
+        subprocess.run([muscle_exe, "-align", fasta_path, "-output", output_fasta], 
+                       check=True,
+                       stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL)
         alignment = AlignIO.read(output_fasta, "fasta")
+
     except subprocess.CalledProcessError as e:
         print(f"Error with MUSCLE command: {e.stderr}")
         sys.exit(1)
-    
+
     return alignment
 
 
-def calculate_henikoff_weights(alignment):
+def create_alignment_order_map(alignment) -> dict:
+    alignment_name_to_row = {rec.id: i for i, rec in enumerate(alignment)}
+    return alignment_name_to_row
+
+
+def calculate_henikoff_weights(alignment, session):
+    protein_to_alignment_row = create_alignment_order_map(alignment)
+
     n_seqs = len(alignment)
     alignment_length = alignment.get_alignment_length()
-    henikoff_weights = [0.0] * n_seqs
+    raw_weights = [0.0] * n_seqs
 
     for i in range(alignment_length):
         column = alignment[:, i]
@@ -171,12 +187,31 @@ def calculate_henikoff_weights(alignment):
         for seq_index, res in enumerate(column_lst):
             if res == "-":
                 continue
-            henikoff_weights[seq_index] += 1 / (n_distinct * column_lst.count(res))
+            raw_weights[seq_index] += 1 / (n_distinct * column_lst.count(res))
 
-    total = sum(henikoff_weights)
-    henikoff_weights = [w / total for w in henikoff_weights]
+    total = sum(raw_weights)
+    henikoff_weights = [
+        raw_weights[protein_to_alignment_row[protein.file_name]] / total
+        for protein in session.proteins
+    ]
 
     return henikoff_weights
+
+
+def generate_sequence_alignment_maps(alignment, session):
+    protein_to_alignment_row = create_alignment_order_map(alignment)
+
+    for protein in session.proteins:
+        alignment_row = alignment[protein_to_alignment_row[protein.file_name], :]
+        seq_to_align_map = {}
+
+        seq_index = 0
+        for align_index, res in enumerate(alignment_row):
+            if res != "-":
+                seq_to_align_map[seq_index] = align_index
+                seq_index += 1
+        
+        protein.sequence_to_alignment_map = seq_to_align_map
 
 
 def calculate_seq_conservation(alignment):
@@ -206,26 +241,27 @@ def calculate_plddt_scores(session, mean, sd):
         protein.PLDDT_scores = plddt_scores
 
 
-# def calculate_plddt_conservation(henikoff_weights, alignment, session):
-#     alignment_length = alignment.get_alignment_length()
+def calculate_plddt_conservation(henikoff_weights, alignment, session):
+    alignment_length = alignment.get_alignment_length()
+    plddt_conservation = [0.0] * alignment_length
+    weight_sums = [0.0] * alignment_length
 
-#     plddt_per_column = []
+    for row, protein in enumerate(session.proteins):
+        scores = protein.PLDDT_per_res
+        seq_to_align_map = protein.sequence_to_alignment_map
+        weight = henikoff_weights[row]
 
-#     for i in range(alignment_length):
-#         column = alignment[:, i]
-#         weight_sum = 0
-#         weighted_score = 0
-        
-#         for index, res in enumerate(list(column)):
-#             if res == "-":
-#                 continue
-#             weight = henikoff_weights[index]
-#             protein = session.proteins[index]
-#             weighted_score += protein.PLDDT_scores[] * weight
-#             weight_sum += weight
-#         plddt_per_column[i] = weighted_score / weight_sum
+        for index, score in enumerate(scores):
+            alignment_index = seq_to_align_map[index]
+            plddt_conservation[alignment_index] += score * weight
+            weight_sums[alignment_index] += weight
 
-#     return plddt_per_column
+    plddt_conservation = [
+        total / weight_sums[i] if weight_sums[i] > 0 else 0.0
+        for i, total in enumerate(plddt_conservation)
+    ]
+
+    return plddt_conservation
 
 
 def make_ds_file(work_dir, session):
@@ -242,7 +278,9 @@ def make_ds_file(work_dir, session):
 def p2rank_command(work_dir, session, p2rank_path, threads):
     work_dir = Path(work_dir)
     ds_path = work_dir / f"{session.name}_p2rank.ds"
-    output_path = work_dir / "Output"
+    output_path = work_dir.joinpath("P2Rank_Output")
+
+    session.P2Rank_output_path = output_path
 
     try:
         output_path.mkdir(exist_ok=True)
@@ -264,10 +302,10 @@ def p2rank_command(work_dir, session, p2rank_path, threads):
 
 def parse_p2rank_output(work_dir, session, aa_map):
     work_dir = Path(work_dir)
-    output_path = work_dir.joinpath("Output")
+    output_path = work_dir.joinpath("P2Rank_Output")
 
     for protein in session.proteins:
-        path = f"{protein.protein_name}.cif_residues.csv"
+        path = f"{protein.file_name}.cif_residues.csv"
         full_path = output_path.joinpath(path)
 
         with open(full_path, "r") as f:
@@ -277,20 +315,36 @@ def parse_p2rank_output(work_dir, session, aa_map):
         protein.P2Rank_per_res = list(res_scores)
 
 
-def calculate_p2rank_conservation(henikoff_weights, alignment, session):
-    pass
-    # use alignment to map per protein score to aligned residues if "-" then 0
-    # normalize each row
-    # use henikoff_weights for each row to get final p2rank score for aligned residues
-    # return p2rank score
-
-
 def normalize(scores):
     x_min = min(scores)
     x_max = max(scores)
     if x_max == x_min:
-        return [0.0] * len(scores)
+        return [0] * len(scores)
     return [(x - x_min) / (x_max - x_min) for x in scores]
+
+
+def calculate_p2rank_conservation(henikoff_weights, alignment, session):
+    p2rank_conservation = [0.0] * alignment.get_alignment_length()
+    weight_sums = [0.0] * alignment.get_alignment_length()
+
+    for row_index, protein in enumerate(session.proteins):
+        p2rank_score = protein.P2Rank_per_res
+        norm_p2rank_score = normalize(p2rank_score)
+        seq_to_align_map = protein.sequence_to_alignment_map
+        weight = henikoff_weights[row_index]
+
+        
+        for index, score in enumerate(norm_p2rank_score):
+            alignment_index = seq_to_align_map[index]
+            p2rank_conservation[alignment_index] += score * weight
+            weight_sums[alignment_index] += weight
+
+    p2rank_conservation = [
+        total / weight_sums[i] if weight_sums[i] > 0 else 0.0
+        for i, total in enumerate(p2rank_conservation)
+    ]
+
+    return p2rank_conservation
 
 
 def calculate_final_score(session, sequence_weight, plddt_weight, p2rank_weight):
@@ -305,6 +359,78 @@ def calculate_final_score(session, sequence_weight, plddt_weight, p2rank_weight)
     ]
     
     return normalize(final_score)
+
+
+def reverse_dict(given_dict):
+    return_dict = {}
+    for key in given_dict.keys():
+        val = given_dict[key]
+        return_dict[val] = key
+
+    return return_dict
+
+
+def map_final_score_to_proteins(session):
+    final_score = session.final_score
+
+    for protein in session.proteins:
+        final_score_per_res = [0.0] * len(protein.sequence)
+        map = protein.sequence_to_alignment_map
+        reverse_map = reverse_dict(map)
+
+        for index, score in enumerate(final_score):
+            if index in reverse_map.keys():
+                final_score_per_res[reverse_map[index]] += score
+
+        protein.final_score_per_res = final_score_per_res
+
+
+def prepare_output(output_dir, work_dir, session):
+
+    output_dir = Path(output_dir)
+    work_dir = Path(work_dir)
+
+    msa_path = Path(session.MSA_path)
+    new_msa_path = output_dir.joinpath(f"{session.name}_msa.fasta")
+
+    p2rank_output_path = session.P2Rank_output_path
+    new_p2rank_output_path = output_dir.joinpath(f"{session.name}_P2Rank_output")
+
+    program_file_path = output_dir.joinpath(f"{session.name}_summary.json")
+    
+    protein_files = [
+        {
+            "file_name": protein.file_name,
+            "sequence": protein.sequence,
+            "PLDDT_per_residue": protein.PLDDT_per_res,
+            "PLDDT_score_per_residue": protein.PLDDT_scores,
+            "P2Rank_per_residue": protein.P2Rank_per_res,
+            "final_score_per_residue": protein.final_score_per_res
+        } for protein in session.proteins
+    ]
+
+    result = {
+        "final_score": session.final_score,
+        "sequence_conservation": session.sequence_conservation,
+        "PLDDT_score_conservation": session.PLDDT_score_conservation,
+        "P2Rank_score_conservation": session.P2Rank_score_conservation,
+        "protein_files": protein_files 
+    }
+
+    root = {"result": result}
+
+    with open(program_file_path, "w") as out:
+        json.dump(root, out, indent=4, sort_keys=True)
+
+    subprocess.run(["mv", msa_path, new_msa_path], check=True)
+    subprocess.run(["mv", p2rank_output_path, new_p2rank_output_path])
+    
+    for item in work_dir.iterdir():
+            if item.is_file() or item.is_symlink():
+                item.unlink()
+            elif item.is_dir():
+                shutil.rmtree(item)
+
 
 
 # -------------------------
@@ -324,9 +450,10 @@ def main():
     # 3. Generate MSA
     alignment = muscle_command(WORK_DIR, MUSCLE_EXE, session)
     session.MSA_path = Path(WORK_DIR).joinpath("muscle_input.fasta")
+    generate_sequence_alignment_maps(alignment, session)
 
     # 4. Calculate Henikoff weighting (Bio.Align.MultipleSeqAlignment)
-    henikoff_weights = calculate_henikoff_weights(alignment)
+    henikoff_weights = calculate_henikoff_weights(alignment, session)
 
     # 5. Use BLOSSUM62 matrix for seq conservation score
     seq_conservation = calculate_seq_conservation(alignment)
@@ -335,29 +462,32 @@ def main():
     # 6. Calculate PLDDT scores via Guassian Weight
     calculate_plddt_scores(session, PLDDT_MEAN, PLDDT_SD)
 
-    # # 7. PLDDT score merged via MSA and Henikoff weights
-    # plddt_conservation = calculate_plddt_conservation(henikoff_weights, alignment, session)
-    # session.PLDDT_score_conservation = plddt_conservation
+    # 8. PLDDT score merged via MSA and Henikoff weights        
+    plddt_conservation = calculate_plddt_conservation(henikoff_weights, alignment, session)
+    session.PLDDT_score_conservation = plddt_conservation
     
-    # 8. make .ds file containing paths to all the pdb/cif files
+    # 9. make .ds file containing paths to all the pdb/cif files
     make_ds_file(WORK_DIR, session)
 
-    # 9. P2Rank prediction
+    # 10. P2Rank prediction
     p2rank_command(WORK_DIR, session, P2RANK_PATH, THREADS)
 
-    # 10. Parse P2Rank output
+    # 11. Parse P2Rank output
     parse_p2rank_output(WORK_DIR, session, AMINO_ACID_MAP)
 
-    # 11. P2Rank score merged via MSA and Henikoff weights
+    # 12. P2Rank score merged via MSA and Henikoff weights
     p2rank_conservation = calculate_p2rank_conservation(henikoff_weights, alignment, session)
     session.P2Rank_score_conservation = p2rank_conservation
 
-    # 12. Final Score Calculation (Final Score = [Seq Conservation]^a * [PLDDT Score]^b * [P2Rank]^c)
+    # 13. Final Score Calculation (Final Score = [Seq Conservation]^a * [PLDDT Score]^b * [P2Rank]^c)
     final_score = calculate_final_score(session, WEIGHT_SEQUENCE, WEIGHT_PLDDT, WEIGHT_P2RANK)
     session.final_score = final_score
 
-    # 13. Mapping to each residue in original proteins
-    # 14. Prepare output files
+    # 14. Mapping to each residue in original proteins
+    map_final_score_to_proteins(session)
+
+    # 15. Prepare output files
+    prepare_output(OUTPUT_DIR, WORK_DIR, session)
 
 if __name__ == "__main__":
     main()
